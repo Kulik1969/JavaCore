77103fc2b4c3cd925c3b5e40a1bb23833d488c70
Andrey Kulikov <kulik.69@bk.ru>
2020-01-21 20:59:01 +0300

    Homework3

---
+++ src/lesson3/homework3/Task11.java
@@ -0,0 +1,23 @@
+package lesson3.homework3;
+
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = (int) ( Math.random() * 10 );
+        System.out.println("Угдай число с 3 попыток от 0 до 9");
+        for (int i = 3; i > 0; i--) {
+            System.out.println("Введи свой ответ: (для выхода из игры, просто нажмите Enter)");
+            int questionNum = scanner.nextInt(); //try не будем ставить, так как еще не узучали :)
+            if (questionNum == number) {
+                System.out.println("Вы угадали число, молодец!");
+                break;
+            }
+            System.out.println(questionNum > number ? "Введенное чилсо больше " : "Введенное число меньше ");
+            System.out.println((i-1) > 0 ? " у вас осталось " + (i-1) + " попыток." : "Попытки закончились, вы проиграли.");
+        }
+        System.out.println("Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет)");
+    }       while (scanner.nextInt() == 1);
+
+}
